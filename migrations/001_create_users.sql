86aac3ce5c5f84744fb8c38e3f5e7ef92fce5087
Garv-Pandey <pandey.garv25@gmail.com>
2026-06-10 19:44:05 +0530

    add: users table in trainalyse-db and some sample data

---
+++ migrations/001_create_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+-- sample data
+INSERT INTO users (name) VALUES ('Joe');
+INSERT INTO users (name) VALUES ('Mama');
