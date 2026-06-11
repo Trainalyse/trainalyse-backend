6c04374a6966428a15b3671424055467e600ffac
Garv-Pandey <pandey.garv25@gmail.com>
2026-06-11 18:09:56 +0530

    add: CRUD operation endpoints over users table.

--- src/trainalyse-backend/main.py
+++ src/trainalyse-backend/main.py
@@ -2,10 +2,13 @@ from workers import WorkerEntrypoint  # cloudflare native libarary
 import asgi  # for building asyncronous api
 from fastapi import FastAPI, Depends
 from auth import check_dev_token
+from users import router as users_router
 
 app = FastAPI(
     dependencies=[Depends(check_dev_token)]
 )  # only allow access to the api when token is valid
+
+app.include_router(users_router)
 
 
 @app.get("/")
